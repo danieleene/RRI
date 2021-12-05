43039c3b5247ecb125a472036d98f187c64f6d0e
Daniele Ene <danieleene98@outlook.it>
2021-12-05 09:19:44 +0100

    Correcting a possible bug
    In Player 2D movement

--- Run Run Impostor/Assets/PlayerMovement.cs
+++ Run Run Impostor/Assets/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
+        float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
