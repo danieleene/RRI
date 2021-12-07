e78883695c329d160eb033fb8f1653e5370d1cda
Daniele Ene <danieleene98@outlook.it>
2021-12-07 18:30:51 +0100

    Prototype of Running Movement
    Prototype only !
    Animation works (switch between Idle Movement and Running Movement)

--- Run Run Impostor/Assets/PlayerMovement.cs
+++ Run Run Impostor/Assets/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
+    private Animator anim;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,19 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x , 14f);
+        }
+
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
