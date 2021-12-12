3bdb5ca1e1fe673eabdc272c4086b16a2ab46afb
Daniele Ene <danieleene98@outlook.it>
2021-12-12 17:52:25 +0100

    Count my cherries

--- Run Run Impostor/Assets/Scripts/ItemCollector.cs
+++ Run Run Impostor/Assets/Scripts/ItemCollector.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int cherries = 0;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
             Destroy(collision.gameObject);
+            cherries++;
+
         }
     }
 
