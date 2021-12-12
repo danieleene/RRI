d69f2a8d9fb7619bad2ef8efd9f591e0d776f506
Daniele Ene <danieleene98@outlook.it>
2021-12-12 17:49:16 +0100

    Feed me cherries
    My purpose is to eat all cherries

--- Run Run Impostor/Assets/Scripts/ItemCollector.cs
+++ Run Run Impostor/Assets/Scripts/ItemCollector.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
