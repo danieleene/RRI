fdd27bcc0ad9aae64bf8c221c39c045bd5006476
Daniele Ene <danieleene98@outlook.it>
2021-12-13 14:16:34 +0100

    Reloading level in PlayerLife script
    Code for Restart level (world) after player hits a trap and die

--- Run Run Impostor/Assets/Scripts/PlayerLife.cs
+++ Run Run Impostor/Assets/Scripts/PlayerLife.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -25,6 +26,11 @@ public class PlayerLife : MonoBehaviour
     {
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
