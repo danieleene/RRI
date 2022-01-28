eb223d4b584ba9dc0d01b3bca87635992a18cbfb
Daniele Ene <danieleene98@outlook.it>
2022-01-28 18:48:35 +0100

    Implementing Logout + adding C # script to the button

---
+++ Run Run Impostor/Assets/Scripts/FacebookLogout.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FacebookLogout : MonoBehaviour
+{
+    public void FbLogout()
+    {
+        FB.LogOut();
+    }
+}
